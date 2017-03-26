5c23083ed95bf8bb0a3481ac54e66ec1a6b5cad0
yordank <yordankp@gmail.com>
2017-03-26 15:43:35 +0300

    add unit tests

---
+++ SampleChat/ChessWebAppTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SampleChat.Controllers;
+using System.Collections.Generic;
+using SampleChat.Models;
+using System.Linq;
+
+namespace ChessWebAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var controller = new HomeController();
+            var result = controller.ListGames() as ViewResult;
+            Assert.IsInstanceOfType(result.Model, typeof(List<Results>));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            var controller = new HomeController();
+            var result = controller.ListGames() as ViewResult;
+
+            var context = new ChatDbContext();
+
+            var res = (List<Results>)result.Model;
+
+            Assert.AreEqual(res.Count(), context.results.Count());
+        }
+
+
+    }
+}
