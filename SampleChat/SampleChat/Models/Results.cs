0edf985fdcc97a0387dc37736be9bdd178a4e76a
yordank <yordankp@gmail.com>
2017-03-26 14:39:48 +0300

    Results entity

---
+++ SampleChat/SampleChat/Models/Results.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleChat.Models
+{
+    public class Results
+    {
+        public int Id { get; set; }
+        public string WhiteUserName { get; set; }
+        public string BlackUserName { get; set; }
+
+        public string  Result { get; set; }
+    }
+}
