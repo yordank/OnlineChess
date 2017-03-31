f03ec901987eefe949bbddd16610a91267705465
yordank <yordankp@gmail.com>
2017-03-31 07:03:56 +0300

    bug fixed + refactoring

--- SampleChat/SampleChat/Hubs/ChatHub.cs
+++ SampleChat/SampleChat/Hubs/ChatHub.cs
@@ -8,49 +8,60 @@ using Newtonsoft.Json;
 using System.Linq;
 using System.Timers;
 using System.Diagnostics;
+using SampleChat.Hubs;
+using System.Data.Entity;
 
 namespace SignalRChat
 {
 
 
-public class UserConnection
+    public class UserConnection
     {
-      public string UserName { set; get; }
-      public string userId { set; get; }
+        public string UserName { set; get; }
+        public string userId { set; get; }
 
-      public int Time { get; set; }
+        public int Time { get; set; }
 
-      public override bool Equals(object obj)
-      {
+        public override bool Equals(object obj)
+        {
             var other = obj as UserConnection;
             if (other == null)
             {
                 return false;
             }
-            return this.UserName == other.UserName && this.userId== other.userId;
-      }
+            return this.UserName == other.UserName && this.userId == other.userId;
+        }
         public override int GetHashCode()
         {
             return (UserName + userId).GetHashCode();
         }
     }
-public class ChatHub : Hub
+    public class ChatHub : Hub
     {
         public static HashSet<UserConnection> usersWhoSeekOpponent = new HashSet<UserConnection>();
 
-        public static Dictionary<string, OpponentAndTimer> usersOpponent = new Dictionary<string, OpponentAndTimer>();
+        public static Dictionary<string, Game> GamesOnline = new Dictionary<string, Game>();
 
-        public void chatsend(string name ,string message)
+        public void chatsend(string name, string message)
         {
-            string userId = usersOpponent[name].OpponentName;
+            string userId = GamesOnline[name].OpponentName;
 
-            Clients.User(userId).addNewMessageToPage(name,message,0);
+            Clients.User(userId).addNewMessageToPage(name, message, 0);
 
-            Clients.User(name).addNewMessageToPage(name, message,0);
+            Clients.User(name).addNewMessageToPage(name, message, 0);
 
         }
-        public void Send(string name, string moveString,string status)
+        public void Send(string name, string moveString, string status)
         {
+
+            using (var context = new ChatDbContext())
+            {
+
+                context.messages.Add(new Messages(moveString));
+                context.SaveChanges();
+
+            }
+
 
 
             if (status == "Game over, White is in checkmate.")
@@ -58,12 +69,16 @@ public class ChatHub : Hub
                 using (var context = new ChatDbContext())
                 {
 
-                    Results res = new Results() { WhiteUserName = usersOpponent[name].OpponentName, BlackUserName = name, Result = "B" };
-                        context.results.Add(res);
-                        context.SaveChanges();
-                   
+                    Results res = new Results() { WhiteUserName = GamesOnline[name].OpponentName, BlackUserName = name, Result = "B" };
+                    context.results.Add(res);
+
+                    context.Entry(res).State = EntityState.Added;
 
 
+
+                    context.SaveChanges();
+
+                    
                 }
             }
 
@@ -72,40 +87,41 @@ public class ChatHub : Hub
                 using (var context = new ChatDbContext())
                 {
 
-                    context.results.Add(new Results() { WhiteUserName = name, BlackUserName = usersOpponent[name].OpponentName, Result = "W" });
+                    context.results.Add(new Results() { WhiteUserName = name, BlackUserName = GamesOnline[name].OpponentName, Result = "W" });
+
+                   
+
                     context.SaveChanges();
 
-
-
+                   
                 }
             }
 
-            if(status== "Game over, drawn position.")
+            if (status == "Game over, drawn position.")
             {
                 using (var context = new ChatDbContext())
                 {
-                    if (usersOpponent[name].Color == "white")
+                    if (GamesOnline[name].Color == "white")
                     {
-                        context.results.Add(new Results() { WhiteUserName = name, BlackUserName = usersOpponent[name].OpponentName, Result = "D" });
+                        context.results.Add(new Results() { WhiteUserName = name, BlackUserName = GamesOnline[name].OpponentName, Result = "D" });
                         context.SaveChanges();
                     }
                     else
                     {
-                        context.results.Add(new Results() { WhiteUserName = usersOpponent[name].OpponentName, BlackUserName = name, Result = "D" });
+                        context.results.Add(new Results() { WhiteUserName = GamesOnline[name].OpponentName, BlackUserName = name, Result = "D" });
                         context.SaveChanges();
                     }
-
-
+                    
                 }
             }
 
 
-            string userId = usersOpponent[name].OpponentName;
+            string userId = GamesOnline[name].OpponentName;
 
 
-            if (usersOpponent[name].Status == "Lost on Time")
+            if (GamesOnline[name].Status == "Lost on Time")
             {
-                
+
 
                 Clients.User(userId).addNewMessageToPage(name, $"{name} lost on time!", 1);
 
@@ -114,48 +130,42 @@ public class ChatHub : Hub
                 return;
             }
 
-           
 
-            usersOpponent[name].stop();
-            
+
+            GamesOnline[name].stop();
+
 
             var Move = moveString;
-
+            //var Move = move;
             var SendMessage = new
             {
                 ServiceName = "ChessGameMove",
 
                 Data = Move,
 
-                TimeOpponet = usersOpponent[name].aTimer.Interval/1000,
+                TimeOpponet = GamesOnline[name].aTimer.Interval / 1000,
 
-                TimeUser = usersOpponent[userId].aTimer.Interval/1000
+                TimeUser = GamesOnline[userId].aTimer.Interval / 1000
 
             };
 
-            usersOpponent[userId].start();
+            GamesOnline[userId].start();
 
             var jsonChessGameMove = JsonConvert.SerializeObject(SendMessage);
 
             Clients.User(userId).getOpponentMove(jsonChessGameMove);
-   
-        
-            using (var context = new ChatDbContext())
-            {
 
-               context.messages.Add(new Messages(moveString));
-               context.SaveChanges();
 
-            }
+          
 
         }
 
 
-      
 
-        public void Seek(string name,int time)
+
+        public void Seek(string name, int time)
         {
-            
+
             var us = new UserConnection();
             us.UserName = name;
             us.userId = Context.User.Identity.Name;
@@ -165,11 +175,11 @@ public class ChatHub : Hub
 
             usersWhoSeekOpponent.Add(us);
 
-            
+
             Seekall();
-            
+
         }
-        public void Startgame(string username1,string username2,int time)
+        public void Startgame(string username1, string username2, int time)
         {
 
 
@@ -177,12 +187,12 @@ public class ChatHub : Hub
             var user1 = usersWhoSeekOpponent.Where(x => x.UserName == username1).FirstOrDefault();
             var user2 = usersWhoSeekOpponent.Where(x => x.UserName == username2).FirstOrDefault();
 
-            Clients.User(user1.userId).beginGame("white",time*60);
-            Clients.User(Context.User.Identity.Name).beginGame("black",time*60);
+            Clients.User(user1.userId).beginGame("white", time * 60);
+            Clients.User(Context.User.Identity.Name).beginGame("black", time * 60);
 
 
-            usersOpponent[username1]= new OpponentAndTimer(username1, Context.User.Identity.Name,1000*60*time,"white");
-            usersOpponent[username2]=new OpponentAndTimer(username2, user1.userId,1000*60*time,"black");
+            GamesOnline[username1] = new Game(username1, Context.User.Identity.Name, 1000 * 60 * time, "white");
+            GamesOnline[username2] = new Game(username2, user1.userId, 1000 * 60 * time, "black");
 
             usersWhoSeekOpponent.Remove(user1);
             usersWhoSeekOpponent.Remove(user2);
@@ -193,12 +203,12 @@ public class ChatHub : Hub
         public void Seekall()
         {
 
-            var Users = usersWhoSeekOpponent.Select(x => new { Username=x.UserName,Time=x.Time });
+            var Users = usersWhoSeekOpponent.Select(x => new { Username = x.UserName, Time = x.Time });
 
             var responceMessage = new
             {
-                ServiceName="AllUsersWhoSeekOpponet",
-                Data=Users
+                ServiceName = "AllUsersWhoSeekOpponet",
+                Data = Users
             };
 
 
@@ -209,7 +219,7 @@ public class ChatHub : Hub
 
         }
 
-        
+
 
         public override Task OnDisconnected(bool stopCalled)
         {
@@ -221,93 +231,7 @@ public class ChatHub : Hub
 
 
     }
-
-
-    public class OpponentAndTimer
-
-    {
-        public OpponentAndTimer(string Name,string OpponentName,double time,string Color)
-        {
-            aTimer = new System.Timers.Timer();
-            this.aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            aTimer.Interval = time;
-
-            this.sw = new Stopwatch();
-
-            this.OpponentName = OpponentName;
-
-            this.Status = "Playing";
-
-            this.Color = Color;
-
-            this.Name = Name;
-        }
-        public System.Timers.Timer aTimer { get; set; }
-        public Stopwatch sw { get; set; }
-
-        public string Name { get; set; }
-        public string OpponentName { get; set; }
-
-        public string Status { get; set; }
-
-        public string Color { get; set; }
-
-        public void start()
-        {
-            if (this.aTimer.Interval > 0)
-            {
-                sw.Start();
-                aTimer.Start();
-            }
-
-
-        }
-        public void stop()
-        {
-            sw.Stop();
-            if (aTimer.Interval - sw.ElapsedMilliseconds > 0)
-                aTimer.Interval = aTimer.Interval - sw.ElapsedMilliseconds;
-            aTimer.Stop();
-            sw.Reset();
-
-        }
-
-        public void OnTimedEvent(object source, ElapsedEventArgs e)
-        {
-             
-
-
-            aTimer.Stop();
-
-            sw.Stop();
-
-            aTimer.Dispose();
-
-            Status = "Lost on Time";
-
-            using (var context = new ChatDbContext())
-            {
-                if(this.Color=="white")
-                {
-                    context.results.Add(new Results() { WhiteUserName=this.Name,BlackUserName=this.OpponentName,Result="B"});
-                    context.SaveChanges();
-                }
-                else
-                {
-                    context.results.Add(new Results() { WhiteUserName = this.OpponentName, BlackUserName = this.Name, Result = "W" });
-                    context.SaveChanges();
-                }
-
-                
-
-            }
-
-             
-
-        }
-
-
-    }
-
-
 }
+
+
+ 
